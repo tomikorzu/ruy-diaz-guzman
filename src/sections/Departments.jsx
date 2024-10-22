eaf72976ec6f002c777a0a5764dc8d708c2770a6
tomikorzu <tomykorzu@icloud.com>
2024-10-22 19:07:34 -0300

    feat(Departments): add the color when the mouse is over of the button and when the user click  it, go to the pdf

--- src/sections/Departments.jsx
+++ src/sections/Departments.jsx
@@ -2,136 +2,9 @@ import "./styles/departments.css";
 
 import imgDepartments from "../assets/img/render-principal-edificio.jpg";
 
-const Departments = () => {
-  const footerData = [
-    [
-      { color: "#baff82", text: "3 ambientes, frente con 1 patio" },
-      { color: "#ffd982", text: "3 ambientes, contrafrente con 2 patios" },
-      { color: "#ff9f82", text: "3 ambientes, frente con balcón" },
-      { color: "#82fbff", text: "Duplex 3 ambientes con 2 balcones​" },
-    ],
-    [
-      { color: "#ff8282", text: "3 ambientes, contrafrente con balcón" },
-      { color: "#ff82cb", text: "4 ambientes con terraza y 2 balcones" },
-      { color: "#f082ff", text: "2 ambientes con terraza y balcón" },
-    ],
-    [
-      { color: "#ae82ff", text: "2 ambientes con balcón" },
-      { color: "#8295ff", text: "Duplex 3 ambientes con Terraza y Balcón" },
-      { color: "#82c3ff", text: "Duplex 2 ambientes con Terraza y Balcón" },
-    ],
-  ];
+import { footerData, units } from "../utils/variables.js";
 
-  // const units = [
-  //   {
-  //     avaible: true,
-  //     dept: 12,
-  //     floor: [8, 7],
-  //     color: "#82c3ff",
-  //     file: "",
-  //     row: 2,
-  //     col: 1,
-  //   },
-  //   {
-  //     avaible: false,
-  //     dept: 11,
-  //     floor: [8, 7],
-  //     color: "#8295ff",
-  //     file: "",
-  //     row: 2,
-  //     col: 1,
-  //   },
-  //   {
-  //     avaible: true,
-  //     dept: 10,
-  //     floor: [6],
-  //     color: "#8295ff",
-  //     file: "",
-  //     row: 1,
-  //     col: 1,
-  //   },
-  //   {
-  //     avaible: true,
-  //     dept: 9,
-  //     floor: [6, 5],
-  //     color: "#ae82ff",
-  //     file: "",
-  //     row: 2,
-  //     col: 1,
-  //   },
-  //   {
-  //     avaible: true,
-  //     dept: 8,
-  //     floor: [5],
-  //     color: "#f082ff",
-  //     file: "",
-  //     row: 1,
-  //     col: 1,
-  //   },
-  //   {
-  //     avaible: false,
-  //     dept: 7,
-  //     floor: [4],
-  //     color: "#ff82cb",
-  //     file: "",
-  //     row: 1,
-  //     col: 2,
-  //   },
-  //   {
-  //     avaible: false,
-  //     dept: 6,
-  //     floor: [3],
-  //     color: "#ff8282",
-  //     file: "",
-  //     row: 1,
-  //     col: 1,
-  //   },
-  //   {
-  //     avaible: false,
-  //     dept: 5,
-  //     floor: [3],
-  //     color: "#ff9f82",
-  //     file: "",
-  //     row: 1,
-  //     col: 1,
-  //   },
-  //   {
-  //     avaible: false,
-  //     dept: 4,
-  //     floor: [2],
-  //     color: "#ff8282",
-  //     file: "",
-  //     row: 1,
-  //     col: 1,
-  //   },
-  //   {
-  //     avaible: false,
-  //     dept: 3,
-  //     floor: [2],
-  //     color: "#ff9f82",
-  //     file: "",
-  //     row: 1,
-  //     col: 1,
-  //   },
-  //   {
-  //     avaible: false,
-  //     dept: 2,
-  //     floor: [1],
-  //     color: "#ffd982",
-  //     file: "",
-  //     row: 1,
-  //     col: 1,
-  //   },
-  //   {
-  //     avaible: false,
-  //     dept: 1,
-  //     floor: [1],
-  //     color: "#baff82",
-  //     file: "",
-  //     row: 1,
-  //     col: 1,
-  //   },
-  // ];
+const Departments = () => {
   return (
     <>
       <header className="header-dept">
@@ -140,32 +13,41 @@ const Departments = () => {
       <main className="main-dept">
         <img src={imgDepartments} alt="Imagen de departamentos" />
         <div className="parent">
-          <div className="div1 floor"> 8</div>
-          <div className="div2 floor"> 7</div>
-          <div className="div3 floor"> 6</div>
-          <div className="div4 floor"> 5</div>
-          <div className="div5 floor"> 4</div>
-          <div className="div6 floor"> 3</div>
-          <div className="div7 floor"> 2</div>
-          <div className="div8 floor"> 1</div>
-          <div className="div9"></div>
-          <div className="div10 bigger">11</div>
-          <div className="div11"></div>
-          <div className="div12 bigger">12</div>
-          <div className="div13"></div>
-          <div className="div14 bigger">09</div>
-          <div className="div15 normal">10</div>
-          <div className="div16 normal">08</div>
-          <div className="div17"></div>
-          <div className="div18 bigger">07</div>
-          <div className="div19 normal">05 </div>
-          <div className="div20 normal">06 </div>
-          <div className="div21 normal">04 </div>
-          <div className="div22 normal">03 </div>
-          <div className="div23 normal">01 </div>
-          <div className="div24 normal">02 </div>
+          {[8, 7, 6, 5, 4, 3, 2, 1].map((floor, index) => (
+            <div key={`floor -${index}`} className="floor">
+              {floor}
+            </div>
+          ))}
+
+          {units.map((unit) => (
+            <div
+              key={`unit-${unit.dept}`}
+              className={`unit ${
+                unit.row === 2 || unit.col === 2 ? "bigger" : "normal"
+              }`}
+              style={{
+                gridRow: `span ${unit.row}`,
+                gridColumn: `span ${unit.col}`,
+                width: "100%",
+                cursor: "pointer",
+                transition: "all .4s",
+              }}
+              onMouseOver={(e) => {
+                e.target.style.backgroundColor = unit.color;
+              }}
+              onMouseOut={(e) => {
+                e.target.style.backgroundColor = "";
+              }}
+              onClick={() => {
+                window.open(unit.file, "_blank");
+              }}
+            >
+              {unit.dept}
+            </div>
+          ))}
         </div>
       </main>
+
       <footer>
         {footerData.map((group, groupIndex) => (
           <ul key={groupIndex}>
--- src/utils/variables.js
+++ src/utils/variables.js
@@ -1,113 +1,142 @@
-import { userAlert } from "./mainFunctions.js";
+import depto1 from "../assets/files/depto1.pdf";
+import depto2 from "../assets/files/depto2.pdf";
+import depto3 from "../assets/files/depto3.pdf";
+import depto4 from "../assets/files/depto4.pdf";
+import depto5 from "../assets/files/depto5.pdf";
+import depto6 from "../assets/files/depto6.pdf";
+import depto7 from "../assets/files/depto7.pdf";
+import depto8 from "../assets/files/depto8.pdf";
+import depto9 from "../assets/files/depto9.pdf";
+import depto10 from "../assets/files/depto10.pdf";
+import depto11 from "../assets/files/depto11.pdf";
+import depto12 from "../assets/files/depto12.pdf";
 
-export class User {
-  constructor(username, email, password, fullname) {
-    this.username = username;
-    this.email = email;
-    this.password = password;
-    this.fullname = fullname;
-  }
+export const footerData = [
+  [
+    { color: "#baff82", text: "3 ambientes, frente con 1 patio" },
+    { color: "#ffd982", text: "3 ambientes, contrafrente con 2 patios" },
+    { color: "#ff9f82", text: "3 ambientes, frente con balcón" },
+    { color: "#82fbff", text: "Duplex 3 ambientes con 2 balcones​" },
+  ],
+  [
+    { color: "#ff8282", text: "3 ambientes, contrafrente con balcón" },
+    { color: "#ff82cb", text: "4 ambientes con terraza y 2 balcones" },
+    { color: "#f082ff", text: "2 ambientes con terraza y balcón" },
+  ],
+  [
+    { color: "#ae82ff", text: "2 ambientes con balcón" },
+    { color: "#8295ff", text: "Duplex 3 ambientes con Terraza y Balcón" },
+    { color: "#82c3ff", text: "Duplex 2 ambientes con Terraza y Balcón" },
+  ],
+];
 
-  verifyUserName(username) {
-    switch (true) {
-      case !/^.{4,}$/.test(username):
-        userAlert("Alert", "Username must be at least 4 characters long");
-        return false;
-      case !/^\S+$/.test(username):
-        userAlert("Alert", "Username cannot contain spaces");
-        return false;
-      case !/^.{1,20}$/.test(username):
-        userAlert(
-          "Alert",
-          "Username is too long, must be less than 20 characters"
-        );
-        return false;
-      case !/^[A-Za-z][A-Za-z0-9_\.]*$/.test(username):
-        userAlert("Alert", "Username cannot start with a number");
-        return false;
-      case /^(?!\d+$)[A-Za-z0-9_\.]+$/.test(username) === false:
-        userAlert("Alert", "Username cannot contain only numbers");
-        return false;
-      default:
-        return true;
-    }
-  }
-
-  verifyFullName(fullname) {
-    switch (true) {
-      case !/^.{4,}$/.test(fullname):
-        userAlert("Alert", "Full Name must be at least 4 characters long");
-        return false;
-      case !/^[A-Za-z\s]+$/.test(fullname):
-        userAlert(
-          "Alert",
-          "Full Name cannot contain numbers or special characters"
-        );
-        return false;
-      case !/^.{1,50}$/.test(fullname):
-        userAlert("Alert", "Full Name must not exceed 50 characters");
-        return false;
-      case !/^[A-Za-z]+(?:\s[A-Za-z]+)+$/.test(fullname):
-        userAlert("Alert", "Full Name must consist of at least two words");
-        return false;
-      case !/^[A-Za-z]+\s[A-Za-z]+$/.test(fullname):
-        userAlert("Alert", "Full Name must not have extra spaces");
-        return false;
-      default:
-        return true;
-    }
-  }
-
-  verifyEmail(email) {
-    switch (true) {
-      case !/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email):
-        userAlert("Alert", "Invalid email format");
-        return false;
-      case !/^\S+$/.test(email):
-        userAlert("Alert", "Email cannot contain spaces");
-        return false;
-      default:
-        return true;
-    }
-  }
-
-  verifyPassword(password) {
-    switch (true) {
-      case !/^.{8,}$/.test(password):
-        userAlert("Alert", "Password must be at least 8 characters long");
-        return false;
-      case !/^(?=.*[A-Z]).+$/.test(password):
-        userAlert(
-          "Alert",
-          "Password must contain at least one uppercase letter"
-        );
-        return false;
-      case !/^\S+$/.test(password):
-        userAlert("Alert", "Password cannot contain spaces");
-        return false;
-      default:
-        return true;
-    }
-  }
-
-  verifyAll() {
-    if (
-      !this.username.trim() ||
-      !this.email.trim() ||
-      !this.password.trim() ||
-      !this.fullname.trim()
-    ) {
-      userAlert("Alert", "All fields are required");
-      return false;
-    }
-    if (
-      this.verifyUserName(this.username) &&
-      this.verifyFullName(this.fullname) &&
-      this.verifyEmail(this.email) &&
-      this.verifyPassword(this.password)
-    ) {
-      return true;
-    }
-    return false;
-  }
-}
+export const units = [
+  {
+    avaible: false,
+    dept: 11,
+    floor: [8, 7],
+    color: "#8295ff",
+    file: depto11,
+    row: 2,
+    col: 1,
+  },
+  {
+    avaible: true,
+    dept: 12,
+    floor: [8, 7],
+    color: "#82c3ff",
+    file: depto12,
+    row: 2,
+    col: 1,
+  },
+  {
+    avaible: true,
+    dept: 10,
+    floor: [6],
+    color: "#8295ff",
+    file: depto10,
+    row: 1,
+    col: 1,
+  },
+  {
+    avaible: true,
+    dept: 9,
+    floor: [6, 5],
+    color: "#ae82ff",
+    file: depto9,
+    row: 2,
+    col: 1,
+  },
+  {
+    avaible: true,
+    dept: 8,
+    floor: [5],
+    color: "#f082ff",
+    file: depto8,
+    row: 1,
+    col: 1,
+  },
+  {
+    avaible: false,
+    dept: 7,
+    floor: [4],
+    color: "#ff82cb",
+    file: depto7,
+    row: 1,
+    col: 2,
+  },
+  {
+    avaible: false,
+    dept: 5,
+    floor: [3],
+    color: "#ff9f82",
+    file: depto5,
+    row: 1,
+    col: 1,
+  },
+  {
+    avaible: false,
+    dept: 6,
+    floor: [3],
+    color: "#ff8282",
+    file: depto6,
+    row: 1,
+    col: 1,
+  },
+  {
+    avaible: false,
+    dept: 3,
+    floor: [2],
+    color: "#ff9f82",
+    file: depto3,
+    row: 1,
+    col: 1,
+  },
+  {
+    avaible: false,
+    dept: 4,
+    floor: [2],
+    color: "#ff8282",
+    file: depto4,
+    row: 1,
+    col: 1,
+  },
+  {
+    avaible: false,
+    dept: 1,
+    floor: [1],
+    color: "#baff82",
+    file: depto1,
+    row: 1,
+    col: 1,
+  },
+  {
+    avaible: false,
+    dept: 2,
+    floor: [1],
+    color: "#ffd982",
+    file: depto2,
+    row: 1,
+    col: 1,
+  },
+];
