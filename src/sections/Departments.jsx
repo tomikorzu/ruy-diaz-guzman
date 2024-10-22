f0ae8f154b12edc9b15444838fc127c8d5089b0b
tomikorzu <tomykorzu@icloud.com>
2024-10-22 18:18:15 -0300

    feat(departments.css): add departments grid style

--- src/sections/Departments.jsx
+++ src/sections/Departments.jsx
@@ -21,6 +21,117 @@ const Departments = () => {
       { color: "#82c3ff", text: "Duplex 2 ambientes con Terraza y Balcón" },
     ],
   ];
+
+  // const units = [
+  //   {
+  //     avaible: true,
+  //     dept: 12,
+  //     floor: [8, 7],
+  //     color: "#82c3ff",
+  //     file: "",
+  //     row: 2,
+  //     col: 1,
+  //   },
+  //   {
+  //     avaible: false,
+  //     dept: 11,
+  //     floor: [8, 7],
+  //     color: "#8295ff",
+  //     file: "",
+  //     row: 2,
+  //     col: 1,
+  //   },
+  //   {
+  //     avaible: true,
+  //     dept: 10,
+  //     floor: [6],
+  //     color: "#8295ff",
+  //     file: "",
+  //     row: 1,
+  //     col: 1,
+  //   },
+  //   {
+  //     avaible: true,
+  //     dept: 9,
+  //     floor: [6, 5],
+  //     color: "#ae82ff",
+  //     file: "",
+  //     row: 2,
+  //     col: 1,
+  //   },
+  //   {
+  //     avaible: true,
+  //     dept: 8,
+  //     floor: [5],
+  //     color: "#f082ff",
+  //     file: "",
+  //     row: 1,
+  //     col: 1,
+  //   },
+  //   {
+  //     avaible: false,
+  //     dept: 7,
+  //     floor: [4],
+  //     color: "#ff82cb",
+  //     file: "",
+  //     row: 1,
+  //     col: 2,
+  //   },
+  //   {
+  //     avaible: false,
+  //     dept: 6,
+  //     floor: [3],
+  //     color: "#ff8282",
+  //     file: "",
+  //     row: 1,
+  //     col: 1,
+  //   },
+  //   {
+  //     avaible: false,
+  //     dept: 5,
+  //     floor: [3],
+  //     color: "#ff9f82",
+  //     file: "",
+  //     row: 1,
+  //     col: 1,
+  //   },
+  //   {
+  //     avaible: false,
+  //     dept: 4,
+  //     floor: [2],
+  //     color: "#ff8282",
+  //     file: "",
+  //     row: 1,
+  //     col: 1,
+  //   },
+  //   {
+  //     avaible: false,
+  //     dept: 3,
+  //     floor: [2],
+  //     color: "#ff9f82",
+  //     file: "",
+  //     row: 1,
+  //     col: 1,
+  //   },
+  //   {
+  //     avaible: false,
+  //     dept: 2,
+  //     floor: [1],
+  //     color: "#ffd982",
+  //     file: "",
+  //     row: 1,
+  //     col: 1,
+  //   },
+  //   {
+  //     avaible: false,
+  //     dept: 1,
+  //     floor: [1],
+  //     color: "#baff82",
+  //     file: "",
+  //     row: 1,
+  //     col: 1,
+  //   },
+  // ];
   return (
     <>
       <header className="header-dept">
@@ -28,9 +139,32 @@ const Departments = () => {
       </header>
       <main className="main-dept">
         <img src={imgDepartments} alt="Imagen de departamentos" />
-        <article>
-          <ul></ul>
-        </article>
+        <div className="parent">
+          <div className="div1 floor"> 8</div>
+          <div className="div2 floor"> 7</div>
+          <div className="div3 floor"> 6</div>
+          <div className="div4 floor"> 5</div>
+          <div className="div5 floor"> 4</div>
+          <div className="div6 floor"> 3</div>
+          <div className="div7 floor"> 2</div>
+          <div className="div8 floor"> 1</div>
+          <div className="div9"></div>
+          <div className="div10 bigger">11</div>
+          <div className="div11"></div>
+          <div className="div12 bigger">12</div>
+          <div className="div13"></div>
+          <div className="div14 bigger">09</div>
+          <div className="div15 normal">10</div>
+          <div className="div16 normal">08</div>
+          <div className="div17"></div>
+          <div className="div18 bigger">07</div>
+          <div className="div19 normal">05 </div>
+          <div className="div20 normal">06 </div>
+          <div className="div21 normal">04 </div>
+          <div className="div22 normal">03 </div>
+          <div className="div23 normal">01 </div>
+          <div className="div24 normal">02 </div>
+        </div>
       </main>
       <footer>
         {footerData.map((group, groupIndex) => (
